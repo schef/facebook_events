8cbfd11b4fcf73000ca691309f11a4003998fe6e
schef <zvanstefan@gmail.com>
2023-03-07 15:00:08 +0100

    moving

--- driver.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-
-def get_driver(headless=True):
-    opts = Options()
-    opts.headless = True
-    return webdriver.Firefox()
--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-from driver import get_driver
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+
+import credentials
+
+def get_driver(headless=True):
+    opts = Options()
+    opts.headless = True
+    return webdriver.Firefox()
+
+
+def login(driver, email, password):
+    driver.get("https://www.facebook.com/login")
+    driver.find_element(By.ID, "email").send_keys(email)
+    driver.find_element(By.ID, "pass").send_keys(password)
+    driver.find_element(By.TAG_NAME, "form").submit()
+
 
 if __name__ == "__main__":
     driver = get_driver()
+    login(driver, credentials.mail, credentials.password)
