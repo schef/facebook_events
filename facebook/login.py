5d282c793bf325e1fc3597e5b758924b012e55ce
schef <zvanstefan@gmail.com>
2023-04-03 09:44:51 +0200

    removed junk' git pus

--- facebook/login.py
+++
@@ -1,8 +0,0 @@
-from time import sleep
-from selenium.webdriver.common.by import By
-
-def login(driver, email, password):
-    driver.get("https://www.facebook.com/login")
-    driver.find_element(By.ID, "email").send_keys(email)
-    driver.find_element(By.ID, "pass").send_keys(password)
-    driver.find_element(By.TAG_NAME, "form").submit()
